d137d0f52a5cec177fda22c3350d96dfada5709f
milena <138934144+milenaabernardi@users.noreply.github.com>
2024-06-21 01:33:17 -0300

    Create  2386 - Telescópio.cpp

---
+++ 2386 - Telescópio.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){ 
+    int a, n, cont = 0, f;
+    
+    cin >> a >> n;
+    for(int i = 0; i < n; i++){
+        cin >> f;
+        if(f * a >= 40000000)
+            cont++;
+    }
+    cout << cont << endl;
+
+    return 0;
+}

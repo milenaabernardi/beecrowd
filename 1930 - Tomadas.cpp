b8d3a48ec5718748fb4796fae190ce6b31600398
milena <138934144+milenaabernardi@users.noreply.github.com>
2024-06-21 01:25:44 -0300

    Create  1930 - Tomadas.cpp

---
+++ 1930 - Tomadas.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main (){
+    int t1;
+    int t2;
+    int t3;
+    int t4;
+    
+    cin >> t1 >> t2 >> t3 >> t4;
+    cout << t1-1+t2-1+t3-1+t4 << endl;
+ 
+    return 0;
+}
